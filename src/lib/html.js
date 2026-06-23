c406999782585a561e0e15713d78b333965a8222
Tosei-sudo <84396692+Tosei-sudo@users.noreply.github.com>
2026-06-23 15:15:50 +0900

    ci: GitHub Actions による自動ビルド・FTPデプロイを追加 (#11)
    * fix: Proxy.php の CURLOPT_RETURNTRANSFER を referer 有無に関わらず true に固定し TIMEOUT も設定する (#1)
    
    * fix: Vue コンポーネントに getImage() メソッドを追加して個別再試行ボタンを修正する (#3)
    
    * fix: html.js の getImagesByFancyBox にガード節と try/catch を追加してクラッシュを防ぐ (#4)
    
    * fix: package.json に jszip を依存関係として追加する (#5)
    
    * ci: GitHub Actions による自動ビルド・FTPデプロイワークフローを追加
    
    * ci: シークレット名を FTP_SERVER / FTP_USER / FTP_PASSWORD に修正

--- src/lib/html.js
+++ src/lib/html.js
@@ -31,39 +31,47 @@ const htmlAnalyzer = {
         let scriptTags = $("script", dom), img_urls = [];
 
         scriptTags.each((i, e) => {
-            let script = e.innerText;
+            try {
+                let script = e.innerText;
 
-            // replaceAllに対応してないversionの別処理
-            if (script.replaceAll === undefined) {
-                script = script.replace(/(\r\n|\n|\r)/gm, "").replace(/ /g, "").replace(/\t/g, "");
-            } else {
-                script = script.replaceAll(/(\r\n|\n|\r)/gm, "").replaceAll(" ", "").replaceAll("\t", "");
-            }
-
-            if (script.includes("fancybox.open")) {
-                let aryStartPos = script.indexOf("$.revo_fancybox.open([") + 22;
-                if (aryStartPos == 21) {
-                    aryStartPos = script.indexOf("$.fancybox.open([") + 17;
-                };
-                const aryEndPos = script.indexOf("]", aryStartPos);
-                let aryString;
                 // replaceAllに対応してないversionの別処理
                 if (script.replaceAll === undefined) {
-                    aryString = "[" + script.slice(aryStartPos, aryEndPos).replace(/href/g, "\"href\"") + "]";
+                    script = script.replace(/(\r\n|\n|\r)/gm, "").replace(/ /g, "").replace(/\t/g, "");
                 } else {
-                    aryString = "[" + script.slice(aryStartPos, aryEndPos).replaceAll("href", "\"href\"") + "]";
+                    script = script.replaceAll(/(\r\n|\n|\r)/gm, "").replaceAll(" ", "").replaceAll("\t", "");
                 }
 
-                let ary = (new Function("return " + aryString))();
+                if (script.includes("fancybox.open")) {
+                    let aryStartPos = script.indexOf("$.revo_fancybox.open([") + 22;
+                    if (aryStartPos == 21) {
+                        aryStartPos = script.indexOf("$.fancybox.open([") + 17;
+                    };
+                    if (aryStartPos <= 0) return;
 
-                ary.forEach(img => {
-                    img_urls.push({
-                        src: parseURL(baseUrl, img.href),
-                        state: STATE.UNACQUIRED,
-                        blocking: true,
-                        referer: baseUrl,
+                    const aryEndPos = script.indexOf("]", aryStartPos);
+                    if (aryEndPos == -1) return;
+
+                    let aryString;
+                    // replaceAllに対応してないversionの別処理
+                    if (script.replaceAll === undefined) {
+                        aryString = "[" + script.slice(aryStartPos, aryEndPos).replace(/href/g, '"href"') + "]";
+                    } else {
+                        aryString = "[" + script.slice(aryStartPos, aryEndPos).replaceAll("href", '"href"') + "]";
+                    }
+
+                    let ary = (new Function("return " + aryString))();
+
+                    ary.forEach(img => {
+                        img_urls.push({
+                            src: parseURL(baseUrl, img.href),
+                            state: STATE.UNACQUIRED,
+                            blocking: true,
+                            referer: baseUrl,
+                        });
                     });
-                });
+                }
+            } catch (err) {
+                // fancyboxパース失敗はスキップして処理を継続
             }
         });
 
